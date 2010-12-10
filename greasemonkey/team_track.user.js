25f8b4fef4641019a7f88456fcf5db1b6e68bac7
alex_su <alex_su@trend.com.tw>
2010-12-10 19:52:28 +0800

    add grayed out function and refactor the codes.

--- greasemonkey/team_track.user.js
+++ greasemonkey/team_track.user.js
@@ -5,36 +5,65 @@
 // @include       https://adcteamtrack.trend.com.tw/*
 // ==/UserScript==
 
-submitForm = document.forms[0];
-function getFormElement(name) {
+function setFormSelectedValue(submitForm, elementName, elementValue) {
+	var selectedElement;
 	for(i = 0; i < submitForm.length; i++) {
-		if(submitForm.elements[i].name == name) {
-			return submitForm.elements[i];
+		if(submitForm.elements[i].type == "select-one" && submitForm.elements[i].name == elementName) {
+			selectedElement = submitForm.elements[i];
+			break;
 		}
 	}
 	
-	return;
+	if(!selectedElement) {
+		return;
+	}
+	
+	for(i = 0; i < selectedElement.options.length; i++) {
+		option = selectedElement.options[i];
+		if(option.text == elementValue) {
+			selectedElement.value = option.value;
+			break;
+		}
+	}
+	
+	selectedElement.style.color = "gray";
 }
 
-defaultDefectType = "2725"; //Code Defect
-defaultFrequency = "605"; //Always
-defaultPriority = "477"; //P1: Fix Immediately
-defaultSeverity = "472"; //A- Critical
-defaultDefectFoundInStage = "2722"; //Pre-Alpha
-defaultHowFound = "2198"; //Unit Test
-defaultReportFrom = "1206"; //PDG QA
+function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
 
-getFormElement("F330").value = defaultDefectType;
-getFormElement("F330").disabled = true;
-getFormElement("F352").value = defaultFrequency;
-getFormElement("F352").disabled = true;
-getFormElement("F327").value = defaultPriority;
-getFormElement("F327").disabled = true;
-getFormElement("F326").value = defaultSeverity;
-getFormElement("F326").disabled = true;
-getFormElement("F435").value = defaultDefectFoundInStage;
-getFormElement("F435").disabled = true;
-getFormElement("F339").value = defaultHowFound;
-getFormElement("F339").disabled = true;
-getFormElement("F338").value = defaultReportFrom;
-getFormElement("F338").disabled = true;
+var submitForm = document.forms[0];
+var fields = new Array(7);
+fields[0] = {name: "F330", label: "* Defect Type:", defaultValue: "Code Defect"};
+fields[1] = {name: "F352", label: "* Frequency:", defaultValue: "Always"};
+fields[2] = {name: "F327", label: "* Priority:", defaultValue: "P1: Fix Immediately"};
+fields[3] = {name: "F326", label: "* Severity:", defaultValue: "A- Critical"};
+fields[4] = {name: "F435", label: "* Defect Found in Stage:", defaultValue: "Pre-Alpha"};
+fields[5] = {name: "F339", label: "* How Found:", defaultValue: "Unit Test"};
+fields[6] = {name: "F338", label: "* Report From:", defaultValue: "PDG QA"};
+
+if(submitForm)  {
+	for(x = 0; x < fields.length; x++) {
+		setFormSelectedValue(submitForm, fields[x].name, fields[x].defaultValue);
+	}
+}
+
+//set css
+addGlobalStyle("span.reqfilled, td.ttFieldName {color: gray !important;}");
+
+spanElements = document.getElementsByTagName("span");
+for(i = 0; i < spanElements.length; i++) {
+	for(j = 0; j < fields.length; j++) {
+		if(spanElements[i].innerHTML == fields[j].label) {
+			spanElements[i].style.color = "gray";
+			spanElements[i].innerHTML = spanElements[i].innerHTML.substring(2);
+		}
+	}
+}
